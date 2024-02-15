d7df2775c498606159d6be47805272a66a34d952
sharonazrd <sharon.azrad@gmail.com>
2024-02-15 20:02:58 +0200

    initial commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+full_name = "sharon " + "azrad"
+print(full_name)
+another_full_name = "%s%s" % ("efi ", "sasson")
+print(another_full_name)
+fname = "aviel"
+lname = "buskila"
+third_full_name = f"{fname} {lname}"
+print(third_full_name)
+forth_full_name = "{} {}".format(fname, lname)
+print(forth_full_name)
